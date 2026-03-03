ed42e5d655de263311d43d7a7cdb709f7136a5d5
AnTran2806 <duyan20191999@gmail.com>
2026-03-03 23:16:07 +0700

    Practice Git PR1

--- tests/gitPushPractice.ts
+++ tests/gitPushPractice.ts
@@ -3,3 +3,11 @@ import {test} from"@playwright/test"
 test("Git Push Practice", async()=>{
     console.log("Git Push Practice Test")
 })
+
+test("Git Push Practice2", async()=>{
+    console.log("Git Push Practice Test")
+})
+
+test("Git Push Practice3", async()=>{
+    console.log("Git Push Practice Test")
+})
